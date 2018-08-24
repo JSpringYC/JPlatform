b7892d7097e01fd843285db5645467113e33048b
姜永春 <JSpringYC@gmail.com>
2018-08-24 15:12:07 +0800

    项目初始化

---
+++ src/main/java/com/jiangyc/jplatform/JPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.jiangyc.jplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPlatformApplication.class, args);
+	}
+}
